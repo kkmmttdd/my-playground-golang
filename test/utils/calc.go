934c80a003aab7c1d4c7a1461ecca9d68d9a4d24
kkmmttdd <kkmmttdd@gmail.com>
2020-09-07 23:59:41 +0900

    Add: Basic test

---
+++ test/utils/calc.go
@@ -0,0 +1,5 @@
+package utils
+
+func Multiply(x int, y int) int {
+	return x * y
+}
---
+++ test/utils/calc_test.go
@@ -0,0 +1,7 @@
+package utils
+
+import "testing"
+
+func TestMultiply(t *testing.T) {
+	t.Errorf("waaaaaaaaaaaaaaaaaaaa")
+}
